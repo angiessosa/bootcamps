5eb3a6acb0652227aded3e5958caf1e7c74d7e23
angiessosa <160278140+angiessosa@users.noreply.github.com>
2024-10-28 11:29:37 -0500

    Delete entidades directory

--- entidades/student.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Student {
-    numero_id: number;
-    nombre: string;
-    telefono: number;
-    edad: number;
-    constructor(num: number, nom: string, tel: number, ed: number);
-}
